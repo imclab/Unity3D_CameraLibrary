56d94a112eda6eb3267bdf40d50f5bd19a23c552
Alex Jaeger <agjaeger@ualr.edu>
2014-10-22 16:10:57 -0500

    Began Cube Code

---
+++ CameraLibrary/Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	Vector3[] cubePositions = {
+		new Vector3(),
+		new Vector3()
+	};
+
+	Quaternion[] cubeRotations = {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
